14d996ff8df5a4bd930770c903fac7fe1d621896
therealkratika <therealkratikagupta@gmail.com>
2026-09-03 19:27:29 +0530

    feat: json-reporter.ts improved

--- src/json-reporter.ts
+++ src/json-reporter.ts
@@ -44,47 +44,24 @@ interface JsonReportData {
     score: number;
     status: string;
   };
+
+  recommendations: {
+    type: "warning" | "info" | "success";
+    message: string;
+    command?: string;
+  }[];
+
+  fixes: {
+    name: string;
+    fixed: boolean;
+    message: string;
+  }[];
 }
 
 export function printJsonReport(
   data: JsonReportData,
 ): void {
-  const report = {
-    project: {
-      name: data.project.projectName,
-      path: data.project.projectPath,
-    },
-
-    package: {
-      name: data.packageJson.name,
-      framework: data.packageJson.framework,
-    },
-
-    technology: data.technology,
-
-    dependencies: {
-      production: Object.keys(
-        data.packageJson.dependencies,
-      ).length,
-
-      development: Object.keys(
-        data.packageJson.devDependencies,
-      ).length,
-
-      unused: data.dependencyAnalysis.unusedDependencies,
-
-      unusedDev: data.dependencyAnalysis
-        .unusedDevDependencies,
-    },
-
-    checks: data.projectChecks,
-
-    security: data.securityAnalysis,
-
-    health: data.health,
-  };
-
   console.log(
-    JSON.stringify(report, null, 2),
+    JSON.stringify(data, null, 2),
   );
 }
