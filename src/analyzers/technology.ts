720d948ed5cdcab6a00fa12f5569446fcd3c96c4
therealkratika <therealkratikagupta@gmail.com>
2026-09-04 20:39:42 +0530

    improve technology.ts

--- src/analyzers/technology.ts
+++ src/analyzers/technology.ts
@@ -21,24 +21,44 @@ export function analyzeTechnology(
 
   let framework = "Unknown";
 
-  if (allDependencies.next) {
+  if (has(allDependencies, "next")) {
     framework = "Next.js";
-  } else if (allDependencies.nuxt) {
+  } else if (has(allDependencies, "nuxt")) {
     framework = "Nuxt";
-  } else if (allDependencies["@angular/core"]) {
+  } else if (has(allDependencies, "@angular/core")) {
     framework = "Angular";
-  } else if (allDependencies["@sveltejs/kit"]) {
+  } else if (has(allDependencies, "@sveltejs/kit")) {
     framework = "SvelteKit";
-  } else if (allDependencies.react) {
-    framework = "React";
-  } else if (allDependencies.vue) {
-    framework = "Vue";
-  } else if (allDependencies.svelte) {
-    framework = "Svelte";
-  } else if (allDependencies["@nestjs/core"]) {
+  } else if (has(allDependencies, "@remix-run/react")) {
+    framework = "Remix";
+  } else if (has(allDependencies, "astro")) {
+    framework = "Astro";
+  } else if (has(allDependencies, "gatsby")) {
+    framework = "Gatsby";
+  } else if (has(allDependencies, "@nestjs/core")) {
     framework = "NestJS";
-  } else if (allDependencies.express) {
+  } else if (has(allDependencies, "express")) {
     framework = "Express";
+  } else if (has(allDependencies, "fastify")) {
+    framework = "Fastify";
+  } else if (has(allDependencies, "koa")) {
+    framework = "Koa";
+  } else if (has(allDependencies, "hapi")) {
+    framework = "Hapi";
+  } else if (has(allDependencies, "react")) {
+    framework = "React";
+  } else if (has(allDependencies, "vue")) {
+    framework = "Vue";
+  } else if (has(allDependencies, "svelte")) {
+    framework = "Svelte";
+  } else if (has(allDependencies, "solid-js")) {
+    framework = "SolidJS";
+  } else if (has(allDependencies, "preact")) {
+    framework = "Preact";
+  } else if (has(allDependencies, "electron")) {
+    framework = "Electron";
+  } else if (has(allDependencies, "react-native")) {
+    framework = "React Native";
   }
 
   // --------------------------------
@@ -47,7 +67,7 @@ export function analyzeTechnology(
 
   let language = "JavaScript";
 
-  if (allDependencies.typescript) {
+  if (has(allDependencies, "typescript")) {
     language = "TypeScript";
   }
 
@@ -57,14 +77,28 @@ export function analyzeTechnology(
 
   let styling = "Unknown";
 
-  if (allDependencies.tailwindcss) {
+  if (has(allDependencies, "tailwindcss")) {
     styling = "Tailwind CSS";
-  } else if (allDependencies["sass"]) {
-    styling = "Sass";
-  } else if (allDependencies["styled-components"]) {
+  } else if (has(allDependencies, "bootstrap")) {
+    styling = "Bootstrap";
+  } else if (has(allDependencies, "@chakra-ui/react")) {
+    styling = "Chakra UI";
+  } else if (has(allDependencies, "@mui/material")) {
+    styling = "Material UI";
+  } else if (has(allDependencies, "antd")) {
+    styling = "Ant Design";
+  } else if (has(allDependencies, "styled-components")) {
     styling = "styled-components";
-  } else if (allDependencies["@emotion/react"]) {
+  } else if (has(allDependencies, "@emotion/react")) {
     styling = "Emotion";
+  } else if (has(allDependencies, "sass")) {
+    styling = "Sass";
+  } else if (has(allDependencies, "less")) {
+    styling = "Less";
+  } else if (has(allDependencies, "bulma")) {
+    styling = "Bulma";
+  } else if (has(allDependencies, "materialize-css")) {
+    styling = "Materialize CSS";
   }
 
   // --------------------------------
@@ -74,44 +108,87 @@ export function analyzeTechnology(
   let database = "Unknown";
 
   if (
-    allDependencies.mongodb ||
-    allDependencies.mongoose
+    has(allDependencies, "mongoose") ||
+    has(allDependencies, "mongodb")
   ) {
     database = "MongoDB";
   } else if (
-    allDependencies.pg ||
-    allDependencies.postgres
+    has(allDependencies, "pg") ||
+    has(allDependencies, "postgres")
   ) {
     database = "PostgreSQL";
   } else if (
-    allDependencies.mysql ||
-    allDependencies.mysql2
+    has(allDependencies, "mysql") ||
+    has(allDependencies, "mysql2")
   ) {
     database = "MySQL";
-  } else if (allDependencies.sqlite3) {
+  } else if (
+    has(allDependencies, "mariadb")
+  ) {
+    database = "MariaDB";
+  } else if (
+    has(allDependencies, "sqlite3") ||
+    has(allDependencies, "better-sqlite3")
+  ) {
     database = "SQLite";
-  } else if (allDependencies.redis) {
+  } else if (
+    has(allDependencies, "redis") ||
+    has(allDependencies, "ioredis")
+  ) {
     database = "Redis";
+  } else if (
+    has(allDependencies, "cassandra-driver")
+  ) {
+    database = "Cassandra";
+  } else if (
+    has(allDependencies, "oracledb")
+  ) {
+    database = "Oracle";
+  } else if (
+    has(allDependencies, "mssql")
+  ) {
+    database = "Microsoft SQL Server";
   }
 
   // --------------------------------
-  // ORM
+  // ORM / ODM
   // --------------------------------
 
   let orm = "None";
 
-  if (allDependencies["@prisma/client"]) {
+  if (
+    has(allDependencies, "prisma") ||
+    has(allDependencies, "@prisma/client")
+  ) {
     orm = "Prisma";
-  } else if (allDependencies.prisma) {
-    orm = "Prisma";
-  } else if (allDependencies.sequelize) {
-    orm = "Sequelize";
-  } else if (allDependencies.typeorm) {
-    orm = "TypeORM";
-  } else if (allDependencies["drizzle-orm"]) {
+  } else if (
+    has(allDependencies, "drizzle-orm")
+  ) {
     orm = "Drizzle";
-  } else if (allDependencies.mongoose) {
+  } else if (
+    has(allDependencies, "typeorm")
+  ) {
+    orm = "TypeORM";
+  } else if (
+    has(allDependencies, "sequelize")
+  ) {
+    orm = "Sequelize";
+  } else if (
+    has(allDependencies, "mongoose")
+  ) {
     orm = "Mongoose";
+  } else if (
+    has(allDependencies, "knex")
+  ) {
+    orm = "Knex";
+  } else if (
+    has(allDependencies, "objection")
+  ) {
+    orm = "Objection.js";
+  } else if (
+    has(allDependencies, "mikro-orm")
+  ) {
+    orm = "MikroORM";
   }
 
   return {
@@ -121,5 +198,19 @@ export function analyzeTechnology(
     database,
     orm,
   };
+}
+
+// --------------------------------
+// Check dependency existence
+// --------------------------------
+
+function has(
+  dependencies: Record<string, string>,
+  dependency: string,
+): boolean {
+  return Object.prototype.hasOwnProperty.call(
+    dependencies,
+    dependency,
+  );
 }
 
